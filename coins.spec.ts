b26cacfc400e371c23b3645794def57f5589eb3b
LucasFlaquer <lucas.flaquer@gmail.com>
2022-03-09 20:51:09 -0300

    test cases

---
+++ coins.spec.ts
@@ -0,0 +1,23 @@
+describe('User should be able to receive coins convert to crypto or by courses', ()=> {
+  it('should be able to receive coins', () => {
+    const user = new User()
+  })
+  it('should decrease amount of coins when they are used', () => {
+
+  })
+  it('should trade coins for a course if has enought amount of coins', () => {
+    
+  })
+  it('should not trade coins for a course if coins amount arent enought', () => {
+    
+  })
+  it('should not be able to trade coins into a course if already owns it', () => {
+
+  })
+  it('should be able to convert coins into cryptos', () => {
+    
+  })
+  it('should not be able to convert into cryptos if the amount is not enought', () => {
+
+  })
+})
--- sum.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('it should make a sum', () => {
-  it('should sum', () => {
-    const a = 10 + 5
-
-    expect(a).toBe(10)
-  })
-})
