1394a1fbe378c1ac7fd47fe6257aac51e21d3666
daltoe <gustavodaltoe@hotmail.com>
2022-03-16 19:41:27 -0300

    corrige gramatica

--- coins.spec.ts
+++ coins.spec.ts
@@ -8,10 +8,10 @@ describe('User should be able to receive coins convert to crypto or by courses',
   it('should decrease amount of coins when they are used', () => {
 
   })
-  it('should trade coins for a course if has enought amount of coins', () => {
+  it('should trade coins for a course if has enough amount of coins', () => {
     
   })
-  it('should not trade coins for a course if coins amount arent enought', () => {
+  it('should not trade coins for a course if coins amount arent enough', () => {
     
   })
   it('should not be able to trade coins into a course if already owns it', () => {
@@ -20,7 +20,7 @@ describe('User should be able to receive coins convert to crypto or by courses',
   it('should be able to convert coins into cryptos', () => {
     
   })
-  it('should not be able to convert into cryptos if the amount is not enought', () => {
+  it('should not be able to convert into cryptos if the amount is not enough', () => {
 
   })
 })
